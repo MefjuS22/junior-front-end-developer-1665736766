cba14c79e1302b8995ead482ccac408e86715d4f
MefjuS22 <smydamateusz9@gmail.com>
2022-10-14 11:12:36 +0200

    added dummy data

---
+++ src/Data/data.jsx
@@ -0,0 +1,43 @@
+export const data = [
+  {
+    title: 'Application Setup',
+    status: 'completed',
+    bussinessContext: {},
+  },
+  {
+    title: 'Static Books List',
+    status: 'completed',
+    bussinessContext: {
+      title: '',
+      content: '',
+      created_at: '',
+    },
+  },
+  {
+    title: 'Administration Panel',
+    status: 'completed',
+    bussinessContext: {
+      title: '',
+      content: '',
+      created_at: '',
+    },
+  },
+  {
+    title: 'Connect Admin with Frontend',
+    status: 'active',
+    bussinessContext: {
+      title: '',
+      content: '',
+      created_at: '',
+    },
+  },
+  {
+    title: 'Book Review Feature',
+    status: 'not-available',
+    bussinessContext: {
+      title: '',
+      content: '',
+      created_at: '',
+    },
+  },
+];
