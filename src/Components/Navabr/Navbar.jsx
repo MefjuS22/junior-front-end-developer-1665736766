4c20481fb1d53e9842d7e2b4a7894585f90737e3
MefjuS22 <smydamateusz9@gmail.com>
2022-10-27 02:46:48 +0200

    mior design changes

--- src/Components/Navabr/Navbar.jsx
+++ src/Components/Navabr/Navbar.jsx
@@ -1,10 +1,8 @@
 import React from 'react';
 import { NavBar } from './NavbarStyles';
-import Logo from '../../Icons/logo.svg';
 const Navbar = () => {
   return (
     <NavBar>
-      <img src={Logo} alt="logo" />
       <div>
         <img
           src="https://cdn-icons-png.flaticon.com/512/168/168724.png"
